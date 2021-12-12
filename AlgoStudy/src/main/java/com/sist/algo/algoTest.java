6d6fd6611edffdd092efed729401dc6ea3b4daac
unknown <ibisevic@naver.com>
2021-12-12 16:26:51 +0900

    12-12 빅O표기법,이분탐색,큐,스택,덱,힙 공부

---
+++ AlgoStudy/src/main/java/com/sist/algo/algoTest.java
@@ -0,0 +1,26 @@
+package com.sist.algo;
+
+import java.util.Scanner;
+
+public class algoTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//빅O표기법: 대락젹으로 내 알고리즘에 최악에 케이스에서 몇초정도 걸리는지 계산하는 방법
+		//컴퓨터에서 1초란 : 10^9, 알고리즘은 10^8 이내로 짜야됨. 허나 보통 10^6이여야 맞음.
+		//이분탐색, 정렬이 되어있는 배열에서 내가 원하는 값을 찾는 알고리즘. 나누기 2를 계속 해 간다. 그럼 log2
+		//자료구조를 잘 알아야됨. 확실히. 큐(First In First Out), 스택(LastIn LastOut), 덱(양끝에서), 힙(우선순위큐, 트리형태)
+		int a,b;
+		Scanner sc = new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		for(int i=0;i<a;i++) {
+			for(int j=0;j<b/2;j++) {
+				//유하림 알고리즘
+			}
+		}
+		System.out.println("붕어");
+		
+	}
+
+}
